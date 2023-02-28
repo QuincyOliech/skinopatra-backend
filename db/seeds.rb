f6f528309b090c732261e882128fc8037ec4c81e
Linet Oliech <linetaoliech@gmail.com>
2023-02-28 20:07:07 +0300

    created product instance

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
-puts "🌱 Seeding products..."
+puts "🌱 Seeding products..."  
+
+#create prodcut instance 
+p1 = Product.create(
+    title: "",
+    category: "",
+    description: "",
+    price: ,
+    image_url: "https://bit.ly/41BEeqy"
+    
+)
+
 
 puts "✅ Done seeding!"
