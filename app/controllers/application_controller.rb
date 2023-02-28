ced72cae7e07b1fb05d303787b36d34a20aa29fe
Linet Oliech <linetaoliech@gmail.com>
2023-02-28 20:22:56 +0300

    created get methods for products

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,27 @@
 class ApplicationController < Sinatra::Base
     set :default_content_type, 'application/json'
 
-    
+    #get all products
+    get '/products' do
+        products = Product.all
+        products.to_json(
+            include: {
+                reviews: {
+                    include: :user
+                }
+            })
+    end
+
+    #get single product
+    get '/products/:id' do 
+        product = Product.find(params[:id])
+        product.to_json(
+          include: {
+            reviews: {
+              include: :user
+            }
+          }
+        )
+      end
 
 end
