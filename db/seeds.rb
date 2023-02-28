1ae0cef0934fdf1e5d9a0824c036ea44f0fd416b
Linet Oliech <linetaoliech@gmail.com>
2023-02-28 20:39:00 +0300

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,33 @@
 puts "🌱 Seeding products..."  
 
 #create prodcut instance 
+19.times do
+    categoryarr = ["treatments and serums","cleansers","moisturizers","body scrubs & exfoliators","bath & body", "tools & devices", "lip care"]
 p1 = Product.create(
-    title: "",
-    category: "",
-    description: "",
-    price: ,
-    image_url: "https://bit.ly/41BEeqy"
-    
+    title: Faker::Commerce.product_name,
+    category: categoryarr.sample,
+    description: Faker::Lorem.sentence,
+    price: rand(50..150),
+    image_url: "https://bit.ly/41BEeqy"    
 )
-
-#create user instance 
-20.times do
-    u1 = User.create(
-        name: Faker::Name.name
+rand(1..5).times do
+    Review.create(
+        star_rating: rand(1..10),
+        comment: Faker::Lorem.sentence,
+        user_id: rand(1..20),
+        product_id: p1.id
     )
 end
+end
+#create user instance 
+20.times do
+u1 = User.create(
+        name: Faker::Name.name
+ )
+end
+
 
 #create event instance
-e1 = Review.create(
-    star_rating: rand(1..10),
-    comment:"",
-    user_id: u1.id,
-    product_id: p1.id
-)
+
 
 puts "✅ Done seeding!"
